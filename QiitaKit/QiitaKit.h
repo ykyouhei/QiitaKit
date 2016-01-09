3d140876ee1b6a3588b21ffaf93bd28631b1e0a1
ykyouhei <kyouhei.lab@gmail.com>
2016-01-09 16:42:01 +0900

    Initial Commit

---
+++ QiitaKit/QiitaKit.h
@@ -0,0 +1,19 @@
+//
+//  QiitaKit.h
+//  QiitaKit
+//
+//  Created by 山口　恭兵 on 2016/01/09.
+//  Copyright © 2016年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QiitaKit.
+FOUNDATION_EXPORT double QiitaKitVersionNumber;
+
+//! Project version string for QiitaKit.
+FOUNDATION_EXPORT const unsigned char QiitaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QiitaKit/PublicHeader.h>
+
+
